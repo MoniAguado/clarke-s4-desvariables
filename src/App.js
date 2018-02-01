13add4871b3173b24a83f54ee1f56baf6856235f
mluena <maluenarod@gmail.com>
2018-02-01 11:25:14 +0100

    importing images

--- src/App.js
+++ src/App.js
@@ -2,6 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import addButton24px from './images/addButton24px.png';
+import addButton32px from './images/addButton32px.png';
+import addimg from './images/addimg.png';
+import arriba from './images/arriba.png';
+import btnaddimg from './images/btnaddimg.png';
+import btnclose from './images/btnclose.png';
+import btndown from './images/btndown.png';
+import btndownload from './images/btndownload.png';
+import btndownload2 from './images/btndownload2.png';
+import btnfile from './images/btnfile.png';
+import btnfile2 from './images/btnfile2.png';
+import btnprint from './images/btnprint.png';
+import btnprint2 from './images/btnprint2.png';
+import btnprint3 from './images/btnprint3.png';
+import btnshare from './images/btnshare.png';
+import btnshare2 from './images/btnshare2.png';
+import btnshare3 from './images/btnshare3.png';
+import btnview from './images/btnview.jpg';
+import cinema from './images/cinema.png';
+import cinemaSmall from './images/cinemaSmall.png';
+import expand-arrow from './images/expand-arrow.png';
+import expand-button-medium from './images/expand-button-medium.png';
+import expand-button from './images/expand-button.png';
+import facebook from './images/facebook.png';
+import github from './images/github.png';
+import group from './images/group.png';
+import groupMobile from './images/groupMobile.png';
+import instagram from './images/instagram.png';
+import linkedin from './images/linkedin.png';
+import logo-adalab from './images/logo-adalab.png';
+import logofinal from './images/logofinal.png';
+import logoLetras from './images/logoLetras.png';
+import logoLetrasRegular from './images/logoLetrasRegular.png';
+import logoLetrasMaquina from './images/logoLetrasMaquina.png';
+import logoPrueba from './images/logoPrueba.png';
+import menos from './images/menos.png';
+import minbutom from './images/minbutom.png';
+import minus from './images/minus.svg';
+import minus from './images/minus.png';
+import minus32px from './images/minus32px.png';
+import music from './images/music.png';
+import musicSmall from './images/musicSmall.png';
+import read from './images/read.png';
+import readSmall from './images/readSmall.png';
+import sports from './images/sports.png';
+import sportsSmall from './images/sportsSmall.png';
+import travel from './images/travel.png';
+import travelSmall from './images/travelSmall.png';
+import twitter from './images/twitter.png';
+import videogame from './images/videogame.png';
+import videogameSmall from './images/videogameSmall.png'
+import machine from './images/vintage8Machine.png';
+
 class App extends Component {
   render() {
     return (
